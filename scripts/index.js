ca014a1cb6730fb3b2283d8adaf3620663e3b155
kozhvat <kozhvat@mail.ru>
2023-05-21 22:54:16 +0300

    refactoring 0

--- scripts/index.js
+++ scripts/index.js
@@ -40,30 +40,6 @@ let whatMinus;
 let expensesString;
 // -------------------------------------------------------------------------------
 
-// ФУНКЦИЯ ИНИЦИАЛИЗАЦИИ ЛИМИТА
-function initLimit() {
-  const limitFromStorage = parseInt(localStorage.getItem(STORAGE_LABEL_LIMIT));
-  if (limitFromStorage) {
-    currentLimit = limitFromStorage;
-    limitNode.innerText = currentLimit + CURRENCY;
-  } else {
-    currentLimit = BASE_LIMIT;
-    limitNode.innerText = BASE_LIMIT + CURRENCY;
-  }
-}
-// -------------------------------------------------------------------------------
-
-// СЧИТАЕМ СУММУ ТРАТ
-function getTotal() {
-  let sum = 0;
-
-  expenses.forEach(function (expense) {
-    sum += expense.amount;
-  });
-  return sum;
-}
-// -------------------------------------------------------------------------------
-
 // ПЕРВИЧНЫЙ КОД ПРИ ОТКРЫТИИ СТРАНИЦЫ
 init(expenses);
 
@@ -81,6 +57,7 @@ function initHistory() {
   );
   const expensesFromStorage = JSON.parse(expensesFromStorageString);
 
+  // проверка существования массива в памяти браузера
   if (Array.isArray(expensesFromStorage)) {
     expenses = expensesFromStorage;
   } else {
@@ -88,140 +65,19 @@ function initHistory() {
     render();
   }
 }
-
-// СОХРАНЕНИЕ ТРАТ В БРАУЗЕРЕ
-function saveExpensesToStorage() {
-  expensesString = JSON.stringify(expenses);
-  localStorage.setItem(STORAGE_LABEL_EXPENSES, expensesString);
-}
 // -------------------------------------------------------------------------------
 
-// ОСНОВНОЙ КОД (ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ДОБАВИТЬ")
-buttonAddExpenseNode.addEventListener('click', function () {
-  // сохраняем введённую сумму в переменную currentAmount(текущая сумма)
-  const currentAmount = getExpenseFromUser();
-  const currentCategory = getSelectedCategory();
-
-  if (!currentCategory || !currentAmount || currentAmount < 0) {
-    alert('Введите сумму больше 0 руб. и выберите категорию');
-    return;
-  }
-
-  // сохраняем выбранную категорию в переменную currentCategory(текущая категория)
-
-  const newExpense = { amount: currentAmount, category: currentCategory };
-
-  expenses.push(newExpense);
-
-  historyNode.innerHTML = '';
-
-  saveExpensesToStorage();
-
-  getTotal(expenses);
-
-  render();
-
-  clearInput();
-});
-// -------------------------------------------------------------------------------
-
-// ОБЩАЯ ФУНКЦИЯ ОТРИСОВКИ ИСТОРИИ, СУММЫ И СТАТУСА
-function render() {
-  renderHistory(expenses);
-  renderTotal(expenses);
-  renderStatus(expenses);
-}
-// -------------------------------------------------------------------------------
-
-// ОТРИСОВКА ОБЩЕЙ СУММЫ ТРАТ
-function renderTotal() {
-  totalValueNode.innerHTML = `${getTotal(expenses) + CURRENCY}`;
-}
-// -------------------------------------------------------------------------------
-
-// БЕРЁМ ЦИФРУ ТРАТЫ ИЗ ПОЛЯ ВВОДА
-function getExpenseFromUser() {
-  const expense = parseInt(inputAddExpenseNode.value);
-  return expense;
-}
-// -------------------------------------------------------------------------------
-
-function getSelectedCategory() {
-  if (inputAddCategoryNode.value === 'Категория') {
-    return false;
+// ФУНКЦИЯ ИНИЦИАЛИЗАЦИИ ЛИМИТА
+function initLimit() {
+  const limitFromStorage = parseInt(localStorage.getItem(STORAGE_LABEL_LIMIT));
+  if (limitFromStorage) {
+    currentLimit = limitFromStorage;
+    limitNode.innerText = currentLimit + CURRENCY;
   } else {
-    return inputAddCategoryNode.value;
+    currentLimit = BASE_LIMIT;
+    limitNode.innerText = BASE_LIMIT + CURRENCY;
   }
 }
-
-// ОЧИСТИТЬ ПОЛЯ ВВОДА ТРАТЫ И КАТЕГОИИ
-function clearInput() {
-  inputAddExpenseNode.value = '';
-  inputAddCategoryNode.value = 'Категория';
-}
-// -------------------------------------------------------------------------------
-
-// ОТРИСОВКА ИСТОРИИ ТРАТ
-function renderHistory(expenses) {
-  expenses.forEach((expense) => {
-    const historyItem = document.createElement('li');
-
-    historyItem.classList = HISTORY_ITEM_CLASSNAE;
-
-    historyItem.innerText = `${expense.amount + CURRENCY} - ${
-      expense.category
-    }`;
-
-    historyNode.appendChild(historyItem);
-  });
-}
-// -------------------------------------------------------------------------------
-
-// ОТРИСОВКА СТАТУСА
-function renderStatus() {
-  if (getTotal(expenses) <= currentLimit) {
-    statusNode.innerHTML = STATUS_IN_LIMIT;
-    statusNode.classList.remove(STATUS_OUT_OF_LIMIT_CLASSNAME);
-  } else {
-    whatMinus = ` (-${getTotal(expenses) - currentLimit + CURRENCY})`;
-    statusNode.innerHTML = `${STATUS_OUT_OF_LIMIT + whatMinus}`;
-    statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
-  }
-}
-// -------------------------------------------------------------------------------
-
-// ФУНКЦИЯ СБРОСА ТРАТ И ОБНОВЛЕНИЯ ВСЕГО
-buttonResetExpensesNode.addEventListener('click', function () {
-  localStorage.removeItem(STORAGE_LABEL_EXPENSES, expensesString);
-  expenses = [];
-
-  init();
-
-  render();
-});
-// -------------------------------------------------------------------------------
-
-// ОСНОВНОЙ КОД НОВОГО ЛИМИТА (ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ИЗМЕНИТЬ ЛИМИТ")
-buttonChangeLimitNode.addEventListener('click', function () {
-  const newLimit = getNewLimitFromUser();
-
-  if (!newLimit || newLimit < 0) {
-    return;
-  }
-
-  trackNewLimit(newLimit);
-
-  // сохранение лимита в браузере
-  localStorage.setItem(STORAGE_LABEL_LIMIT, newLimit);
-
-  renderNewLimit(newLimit);
-
-  renderStatus(getTotal);
-
-  togglePopup();
-
-  clearLimitInput();
-});
 // -------------------------------------------------------------------------------
 
 // ДОБАВЛЕНИЕ НОВОГО ЛИМИТА В JAVASCRIPT
@@ -242,14 +98,169 @@ function getNewLimitFromUser() {
 }
 // -------------------------------------------------------------------------------
 
+// ОЧИСТКА ПОЛЯ ВВОДА НОВОГО ЛИМИТА
+function clearLimitInput() {
+  inputChangeNode.value = '';
+}
+// -------------------------------------------------------------------------------
+
+// СЧИТАЕМ СУММУ ТРАТ
+function getTotal() {
+  let sum = 0;
+
+  expenses.forEach(function (expense) {
+    sum += expense.amount;
+  });
+  return sum;
+}
+// -------------------------------------------------------------------------------
+
+// ПОЛУЧЕНИЕ ЦИФРЫ ТРАТЫ ИЗ ПОЛЯ ВВОДА
+function getExpenseFromUser() {
+  const expense = parseInt(inputAddExpenseNode.value);
+  return expense;
+}
+// -------------------------------------------------------------------------------
+
+// ПОЛУЧЕНИЕ ВЫБРАННОЙ КАТЕГОРИИ
+function getSelectedCategory() {
+  if (inputAddCategoryNode.value === 'Категория') {
+    return false;
+  } else {
+    return inputAddCategoryNode.value;
+  }
+}
+
+// ОЧИСТИТКА ПОЛЯ ВВОДА ТРАТЫ И КАТЕГОИИ
+function clearInput() {
+  inputAddExpenseNode.value = '';
+  inputAddCategoryNode.value = 'Категория';
+}
+// -------------------------------------------------------------------------------
+
+// ОБЩАЯ ФУНКЦИЯ ОТРИСОВКИ ИСТОРИИ, СУММЫ И СТАТУСА
+function render() {
+  renderHistory(expenses);
+  renderTotal(expenses);
+  renderStatus(expenses);
+}
+// -------------------------------------------------------------------------------
+
 // ОТРИСОВКА НОВОГО ЛИМИТА
 function renderNewLimit() {
   limitNode.innerText = currentLimit + CURRENCY;
 }
 // -------------------------------------------------------------------------------
 
-// ОЧИСТКА ПОЛЯ ВВОДА НОВОГО ЛИМИТА
-function clearLimitInput() {
-  inputChangeNode.value = '';
+// СОХРАНЕНИЕ ТРАТ В БРАУЗЕРЕ
+function saveExpensesToStorage() {
+  expensesString = JSON.stringify(expenses);
+  localStorage.setItem(STORAGE_LABEL_EXPENSES, expensesString);
 }
 // -------------------------------------------------------------------------------
+
+// ОТРИСОВКА ИСТОРИИ ТРАТ
+function renderHistory(expenses) {
+  expenses.forEach((expense) => {
+    const historyItem = document.createElement('li');
+
+    historyItem.classList = HISTORY_ITEM_CLASSNAE;
+
+    historyItem.innerText = `${expense.amount + CURRENCY} - ${
+      expense.category
+    }`;
+
+    historyNode.appendChild(historyItem);
+  });
+}
+// -------------------------------------------------------------------------------
+
+// ОТРИСОВКА ОБЩЕЙ СУММЫ ТРАТ
+function renderTotal() {
+  totalValueNode.innerHTML = `${getTotal(expenses) + CURRENCY}`;
+}
+// -------------------------------------------------------------------------------
+
+// ОТРИСОВКА СТАТУСА
+function renderStatus() {
+  if (getTotal(expenses) <= currentLimit) {
+    statusNode.innerHTML = STATUS_IN_LIMIT;
+    statusNode.classList.remove(STATUS_OUT_OF_LIMIT_CLASSNAME);
+  } else {
+    whatMinus = ` (-${getTotal(expenses) - currentLimit + CURRENCY})`;
+    statusNode.innerHTML = `${STATUS_OUT_OF_LIMIT + whatMinus}`;
+    statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
+  }
+}
+// -------------------------------------------------------------------------------
+
+// ФУНКЦИЯ ОБРАБОТЧИКА НАЖАТИЯ КНОПКИ "ДОБАВИТЬ"
+function addButtonHandler() {
+  // сохраняем введённую сумму в переменную currentAmount(текущая сумма)
+  const currentAmount = getExpenseFromUser();
+  const currentCategory = getSelectedCategory();
+
+  if (!currentCategory || !currentAmount || currentAmount < 0) {
+    alert('Введите сумму больше 0 руб. и выберите категорию');
+    return;
+  }
+
+  // сохраняем выбранную категорию в переменную currentCategory(текущая категория)
+  const newExpense = { amount: currentAmount, category: currentCategory };
+
+  expenses.push(newExpense);
+
+  historyNode.innerHTML = '';
+
+  saveExpensesToStorage();
+
+  getTotal(expenses);
+
+  render();
+
+  clearInput();
+}
+// -------------------------------------------------------------------------------
+
+// ФУНКЦИЯ ОБРАБОТЧИКА НАЖАТИЯ КНОПКИ "ИЗМЕНИТЬ ЛИМИТ"
+function changeLimitHandler() {
+  const newLimit = getNewLimitFromUser();
+
+  if (!newLimit || newLimit < 0) {
+    return;
+  }
+
+  trackNewLimit(newLimit);
+
+  // сохранение лимита в браузере
+  localStorage.setItem(STORAGE_LABEL_LIMIT, newLimit);
+
+  renderNewLimit(newLimit);
+
+  renderStatus(getTotal);
+
+  togglePopup();
+
+  clearLimitInput();
+}
+// -------------------------------------------------------------------------------
+
+// ФУНКЦИЯ ОБРАБОТЧИКА НАЖАТИЯ КНОПКИ СБРОСА ТРАТ И ОБНОВЛЕНИЯ ВСЕГО
+function clearButtonHandler() {
+  localStorage.removeItem(STORAGE_LABEL_EXPENSES, expensesString);
+  expenses = [];
+
+  init();
+
+  render();
+}
+// -------------------------------------------------------------------------------
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ДОБАВИТЬ"
+buttonAddExpenseNode.addEventListener('click', addButtonHandler);
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ СБРОСА ТРАТ И ОБНОВЛЕНИЯ ВСЕГО
+buttonResetExpensesNode.addEventListener('click', clearButtonHandler);
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ИЗМЕНИТЬ ЛИМИТ"
+buttonChangeLimitNode.addEventListener('click', changeLimitHandler);
